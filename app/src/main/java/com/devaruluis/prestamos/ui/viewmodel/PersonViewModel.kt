36fa0e3b400fdc9714de89e68af31f64d410314f
Raul Luis <soyrauluis@gmail.com>
2022-05-08 18:34:08 -0400

    Update view models

--- app/src/main/java/com/devaruluis/prestamos/ui/viewmodel/PersonViewModel.kt
+++ app/src/main/java/com/devaruluis/prestamos/ui/viewmodel/PersonViewModel.kt
@@ -12,12 +12,69 @@ import javax.inject.Inject
 @HiltViewModel
 class PersonViewModel @Inject constructor(val personDao: PersonDao) : ViewModel() {
 
-     val success = MutableLiveData<Boolean>()
+    val success = MutableLiveData<Boolean>()
+    val personLD = MutableLiveData<Person>()
+    val error = MutableLiveData<Exception>()
 
     fun save(person: Person) {
+        if (person.id != null) insert(person)
+        else update(person)
+    }
+
+    fun insert(person: Person) {
         viewModelScope.launch {
-                personDao.insert(person)
+            try {
+                val id = personDao.insertWithTimestamp(person)
+                find(id)
                 success.postValue(true)
+            } catch (e: Exception) {
+                error.postValue(e)
+            }
         }
     }
+
+    fun update(person: Person) {
+        viewModelScope.launch {
+            try {
+                val id = personDao.updateWithTimestamp(person)
+                find(person.id)
+                success.postValue(true)
+            } catch (e: Exception) {
+                error.postValue(e)
+            }
+        }
+    }
+
+    fun find(id: Long) {
+        viewModelScope.launch {
+            try {
+                val found = personDao.find(id)
+                personLD.postValue(found)
+                success.postValue(true)
+            } catch (e: Exception) {
+                error.postValue(e)
+            }
+        }
+    }
+
+    fun deleteCurrentPerson() {
+        delete(personLD.value?.id)
+    }
+
+    fun delete(id: Long?) {
+        viewModelScope.launch {
+            try {
+                if (id != null) {
+                    val found = personDao.find(id)
+                    personDao.delete(found)
+                    success.postValue(true)
+                }
+            } catch (e: Exception) {
+                error.postValue(e)
+            }
+
+        }
+    }
+
+
 }
