a7a8578114b2cabdf3d774804e3be3af0882f246
Raul Luis <soyrauluis@gmail.com>
2022-05-08 18:32:07 -0400

    Update database version

--- app/src/main/java/com/devaruluis/prestamos/data/database/LoansDatabase.kt
+++ app/src/main/java/com/devaruluis/prestamos/data/database/LoansDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.devaruluis.prestamos.data.database.dao.PersonDao
 import com.devaruluis.prestamos.model.Person
 
-@Database(entities = [Person::class], version = 1)
+@Database(entities = [Person::class], version = 6)
 abstract class LoansDatabase : RoomDatabase() {
 
     abstract fun getPersonDao(): PersonDao
