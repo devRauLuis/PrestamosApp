2cecc1cbe2e830152f26080cbd4e208173e42783
Raul Luis <soyrauluis@gmail.com>
2022-05-18 00:11:48 -0400

    Upgrade db and add occupation DAO

--- app/src/main/java/com/devaruluis/prestamos/data/database/LoansDatabase.kt
+++ app/src/main/java/com/devaruluis/prestamos/data/database/LoansDatabase.kt
@@ -2,11 +2,13 @@ package com.devaruluis.prestamos.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.devaruluis.prestamos.data.database.dao.OccupationDao
 import com.devaruluis.prestamos.data.database.dao.PersonDao
+import com.devaruluis.prestamos.model.Occupation
 import com.devaruluis.prestamos.model.Person
 
-@Database(entities = [Person::class], version = 6)
+@Database(entities = [Person::class, Occupation::class], version = 13)
 abstract class LoansDatabase : RoomDatabase() {
-
     abstract fun getPersonDao(): PersonDao
+    abstract fun getOccupationDao(): OccupationDao
 }
