4126eb52fa0fb2dd99b2a1d6aa12af24a765de76
Raul Luis <soyrauluis@gmail.com>
2022-05-18 00:12:42 -0400

    Update to support occupation DAO

--- app/src/main/java/com/devaruluis/prestamos/di/AppModule.kt
+++ app/src/main/java/com/devaruluis/prestamos/di/AppModule.kt
@@ -2,12 +2,14 @@ package com.devaruluis.prestamos.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.devaruluis.prestamos.data.database.LoansDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import java.util.concurrent.Executors
 import javax.inject.Singleton
 
 @Module
@@ -21,9 +23,17 @@ object AppModule {
     fun provideRoom(@ApplicationContext context: Context) = Room.databaseBuilder(
         context,
         LoansDatabase::class.java, LOANS_DATABASE_NAME
-    ).fallbackToDestructiveMigration().build()
+    ).fallbackToDestructiveMigration().setQueryCallback(
+        RoomDatabase.QueryCallback
+        { sqlQuery, bindArgs ->
+            println("SQL Query: $sqlQuery SQL Args: $bindArgs")
+        }, Executors.newSingleThreadExecutor()
+    ).build()
 
     @Provides
     fun providePersonDao(loansDatabase: LoansDatabase) = loansDatabase.getPersonDao()
+
+    @Provides
+    fun provideOccupationDao(loansDatabase: LoansDatabase) = loansDatabase.getOccupationDao()
 
 }
